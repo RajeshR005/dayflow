5e00baea5f9c1155f3a5226bda62953231aadd8e
Rajesh <rajeshr30072002@gmail.com>
2026-02-18 01:01:15 +0530

    Expense Tracker API - Edit Expenses

--- app/api/endpoints/edit_expense.py
+++ app/api/endpoints/edit_expense.py
@@ -1,12 +1,59 @@
-from fastapi import APIRouter,Depends,Form
+from fastapi import APIRouter,Depends,Form,UploadFile,File
 from sqlalchemy.orm import Session,joinedload
 from app.api.deps import get_db,get_current_user
 from app.models import *
-from app.schemas.expenses_schema import UpdateExpenseData
+from app.utils import *
+from decimal import Decimal
+from datetime import date
+import os
 
 
 router=APIRouter(tags=["Expense Tracker"])
 
-@router.put('edit_expense/{expense_id}',description="This Route is for Editing the Expense Data")
-def edit_expense_data(expense_id:int, update_record=UpdateExpenseData,db:Session=Depends(get_db),current_user=Depends(get_current_user)):
-    return{}
+@router.put('/edit_expense/{expense_id}',description="This Route is for Editing the Expense Data")
+def edit_expense_data(expense_id:int, title:str=Form(None),amount:Decimal=Form(None),category:str=Form(None),mode:str=Form(None),exp_date:date=Form(None),exp_time:str=Form(None), update_file:UploadFile=File(None),db:Session=Depends(get_db),current_user=Depends(get_current_user)):
+    
+    try:
+
+        get_expense=db.query(ExpenseTracker).filter(ExpenseTracker.id==expense_id,ExpenseTracker.user_id==current_user.id).options(joinedload(ExpenseTracker.expense_medias).joinedload(ExpenseMedia.medias)).first()
+
+        get_wallet=db.query(Wallet).filter(Wallet.user_id==current_user.id).first()
+        if not get_expense:
+            return{"status":0,"msg":"no data found"}
+        if title:
+            get_expense.exp_title=title
+        if category:
+            get_expense.category=category
+        if mode:
+            get_expense.mode=mode
+        if exp_date:
+            get_expense.exp_date=exp_date
+        if exp_time:
+            get_expense.exp_time=exp_time
+        db.commit()
+        db.refresh(get_expense)
+        if amount:
+            get_expense.amount=amount
+            if get_expense.mode=="cash_in":
+                get_wallet.balance+=amount
+            else:
+                get_expense.amount-=amount
+        updated_path=None
+        if update_file:
+            for exp_media in get_expense.expense_medias:
+                os.remove(exp_media.media.img_path)
+                file_path,file_exe=file_storage(update_file,update_file.filename,sub_folder="expenses_imgs")
+                
+                exp_media.media.img_path=file_path
+                updated_path=file_path
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        if updated_path:
+            os.remove(updated_path)
+    return{
+        "status":1,
+        "msg":"data updated successfully"
+    }
+
+    
